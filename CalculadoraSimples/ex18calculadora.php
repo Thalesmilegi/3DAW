11d4e0e6eea4cfc34d26582c14492b2a720d4d50
Thalesmilegi <38260735+Thalesmilegi@users.noreply.github.com>
2021-05-04 11:13:01 -0300

    Add files via upload

---
+++ CalculadoraSimples/ex18calculadora.php
@@ -0,0 +1,31 @@
+<?php 
+    include("index.html");
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $op = $_POST['op'];
+
+    if($num1 != NULL || $num2 != NULL){
+
+        switch($op){
+            case "soma":
+                $resultado = $num1 + $num2;
+                echo "<font style='color:white; margin-left: 47.7%;'>O resultado da soma é: $resultado </font>";
+            break;
+            case "sub":
+                $resultado = $num1 - $num2;
+                echo "<font style='color:white; margin-left: 47.7%;'>O resultado da subtração é: $resultado </font>";
+            break;
+            case "mult":
+                $resultado = $num1 * $num2;
+                echo "<font style='color:white; margin-left: 47.7%;'>O resultado da multiplição é: $resultado </font>";
+            break;
+            case "div":
+                $resultado = $num1 / $num2;
+                echo "<font style='color:white; margin-left: 47.7%;'>O resultado da divisão é: $resultado </font>";
+            break;
+        }
+    }else{
+        echo "Preencha os campos vázios!";
+    }
+
+?>
